235de0bce88d999aea8e428c86df80a69249f4f2
mirmidon369 <ddespinozac@gmail.com>
2017-06-08 12:22:43 -0400

    habilitacion dompdf desde local work
    habilitacion de dompdf

--- Paracelso/config/app.php
+++ Paracelso/config/app.php
@@ -149,6 +149,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -206,6 +207,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'PDF' => 'Barryvdh\DomPDF\Facade',
 
     ],
 
