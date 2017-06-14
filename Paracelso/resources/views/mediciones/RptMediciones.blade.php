0a0a5a277dcadc5d8a5c56c31ff1b777a7512368
mirmidon369 <ddespinozac@gmail.com>
2017-06-14 19:22:00 -0400

    idem anterior
    idem anterior

--- Paracelso/resources/views/mediciones/RptMediciones.blade.php
+++ Paracelso/resources/views/mediciones/RptMediciones.blade.php
@@ -1,16 +1,14 @@
-<html lang="en">
-  <head>
-    <meta http-equiv="Content-Type" "Content-type: image/jpeg" content="text/html; charset=utf-8"/>
-        <title>REPORTE DE MEDICIONES</title>
-    <!-- {!! Html::style('assets/css/pdf.css') !!} -->
-  </head>
-  <body>
-          <h1><center>REPORTE DE SIGNOS VITALES </center> </h1>
-          <div class="date">Fecha de Reporte: {{ $date }}</div>
-      @include('persona.LstDatosBasicos')
-    
-        <table class="table table-bordered table-condensed tabla_small table-fit">
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<!--<img src="snake.jpg">-->
+@include('persona.LstDatosBasicos')
+<table>
             <thead>
+                <tr>
                 <th>Fecha</th>
                 <th>Peso</th>
                 <th>Talla</th>
@@ -21,6 +19,7 @@
                 <th>Temp.°C</th>
                 <th>SPO2</th>
                 <th>Dolor</th>
+                </tr>
             </thead>
             <tbody>
                 @foreach($mediciones as $medicion)
@@ -38,6 +37,6 @@
                 </tr>
                 @endforeach
             </tbody>
-        </table>
-    </body>
+</table>
+</body>
 </html>
